ebadd94669d4aec73bf5e7280470ea01602bf210
Yashraj Raj <147085086+yv18@users.noreply.github.com>
2025-04-23 12:23:34 -0400

    Update socket.js

--- FrontEnd/SRC/Components/socket.js
+++ FrontEnd/SRC/Components/socket.js
@@ -1,5 +1,5 @@
 // socket.js
 import { io } from 'socket.io-client';
-const socket = io('http://localhost:5000');
+const socket = io('https://gc-backend-1ksm.onrender.com');
 export default socket;
 socket.on('connect', () => { console.log('Connected to the server'); });
